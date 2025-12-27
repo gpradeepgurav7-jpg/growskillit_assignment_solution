a2426d67c631a3a9e4c5377ee88a0c6a0ee081fc
pradeep <pradeep@mail.com>
2025-12-27 13:26:58 +0530

    new one added

---
+++ If_Else_Assignment3.java
@@ -0,0 +1,20 @@
+package assignment;
+
+public class If_Else_Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int marks1=47;
+		if(marks1 >=90 & marks1 <=100)
+			System.out.println("Grade A:"+marks1);
+		else if(marks1 >=75 & marks1<=90)
+			System.out.println("Grade B:"+marks1);
+		else if(marks1 >=50 & marks1 <=74)
+			System.out.println("Grade C:"+marks1);
+		else
+			System.out.println("Grade Fail:"+marks1);
+				
+
+	}
+
+}
